8b1b7273d728bec827a4f4a98f4b0602bdfa1418
elCortez <evgenylebedevv@gmail.com>
2024-02-27 19:02:15 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<div id="testdiv" style="padding: 300px;">
+
+<div style="display: flex; flex-wrap: nowrap; flex-direction: row; align-items: center; justify-content: center">
+    <div style='width: 300px; border-radius: 3px; font-size: 20px; margin: 25px; height: 74px; line-height: 74px; text-align: center; text-justify: auto; background-color: #c82333; color: #ffffff; cursor: pointer; font-weight: bold;' id="first">Скрыть</div>
+
+    <div style='width: 300px; border-radius: 3px; font-size: 20px; margin: 25px; height: 74px; line-height: 74px; text-align: center; text-justify: auto; background-color: #47c823; color: #ffffff; font-weight: bold; cursor: pointer;' id="second">Показать</div>
+</div>
+
+    <div id="third">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus architecto at blanditiis debitis fugiat harum minus, nemo omnis quia, quis saepe veniam voluptatem. Consequatur, porro, velit. Ipsam laboriosam, ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus architecto at blanditiis debitis fugiat harum minus, nemo omnis quia, quis saepe veniam voluptatem. Consequatur, porro, velit. Ipsam laboriosam, ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus architecto at blanditiis debitis fugiat harum minus, nemo omnis quia, quis saepe veniam voluptatem. Consequatur, porro, velit. Ipsam laboriosam, ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. </div>
+
+</div>
